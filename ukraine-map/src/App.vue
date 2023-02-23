f4356754c68f3b176034e0dac3807f4740019a93
Marina_Pasternak <256pasternakmarina@gmail.com>
2023-02-23 02:56:55 +0200

    added styles to navigationbar

--- ukraine-map/src/App.vue
+++ ukraine-map/src/App.vue
@@ -1,8 +1,16 @@
 <template>
   <div id="app">
     <div>
-      <router-link to="/posts">Posts</router-link>
-      <router-link to="/ukraine-map">Map</router-link>
+      <b-navbar toggleable="lg" type="light" variant="light">
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" align="center" is-nav>
+          <b-navbar-nav>
+            <router-link to="/posts" class="nav-link">Posts</router-link>
+            <router-link to="/ukraine-map" class="nav-link">Map</router-link>
+          </b-navbar-nav>
+        </b-collapse>
+      </b-navbar>
     </div>
     <router-view />
   </div>
@@ -14,4 +22,20 @@ export default {
 };
 </script>
 
-<style lang="css" scoped></style>
+<style lang="css" scoped>
+.nav-link {
+  padding: 1rem;
+  color: rgb(85, 85, 85);
+  font-weight: 500;
+  font-size: 18pt;
+}
+
+.navbar {
+  padding: 0.5rem;
+  background-color: rgb(210, 210, 210);
+}
+
+.navbar .router-link-active {
+  color: var(--bs-pagination-active-bg);
+}
+</style>
