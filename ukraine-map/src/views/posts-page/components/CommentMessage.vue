ceb225ffd55adbea1af4492ce76ccbd889faf17b
Marina_Pasternak <256pasternakmarina@gmail.com>
2023-02-24 03:06:17 +0200

    added icon in front of comments count

--- ukraine-map/src/views/posts-page/components/CommentMessage.vue
+++ ukraine-map/src/views/posts-page/components/CommentMessage.vue
@@ -2,7 +2,22 @@
   <div>
     <div v-if="commentsForPost && commentsForPost.length > 0">
       <p class="comments-count">
-        <span>All Comments </span>{{ commentsForPost.length }}
+        <span
+          ><svg
+            id="Layer_1"
+            data-name="Layer 1"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 512 512"
+          >
+            <path
+              class="cls-1"
+              d="M17.92,448.24a5,5,0,0,1-5.12-5V115A51.26,51.26,0,0,1,64,63.76H448A51.26,51.26,0,0,1,499.2,115v230.4a51.26,51.26,0,0,1-51.2,51.2H71.5L21.32,446.74A4.65,4.65,0,0,1,17.92,448.24Z"
+            />
+            <path
+              class="cls-2"
+              d="M448,76.56A38.44,38.44,0,0,1,486.4,115v230.4a38.44,38.44,0,0,1-38.4,38.4H66.2l-7.5,7.5-33.1,33.1V115A38.44,38.44,0,0,1,64,76.56H448M448,51H64A64,64,0,0,0,0,115V443.21A17.78,17.78,0,0,0,17.92,461a17.42,17.42,0,0,0,12.45-5.25L76.8,409.36H448a64,64,0,0,0,64-64V115a64,64,0,0,0-64-64Z"
+            /></svg></span
+        >{{ commentsForPost.length }}
       </p>
       <div v-for="item in commentsForPost" :key="item.id">
         <p class="users-email">
@@ -66,5 +81,22 @@ export default {
 .users-email svg {
   width: 50px;
   height: 50px;
+}
+
+.users-email svg {
+  width: 50px;
+  height: 50px;
+}
+
+.comments-count .cls-1 {
+  fill: white;
+}
+.comments-count .cls-2 {
+  fill: #606161;
+}
+
+.comments-count svg {
+  width: 30px;
+  margin-right: 5px;
 }
 </style>
