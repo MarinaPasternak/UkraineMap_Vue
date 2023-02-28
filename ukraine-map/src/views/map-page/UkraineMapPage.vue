52e9216173651a9d880057298a3dfa412ce0daef
Marina_Pasternak <256pasternakmarina@gmail.com>
2023-03-01 00:13:33 +0200

    add markers array

--- ukraine-map/src/views/map-page/UkraineMapPage.vue
+++ ukraine-map/src/views/map-page/UkraineMapPage.vue
@@ -3,6 +3,23 @@
     <h1>MAP</h1>
     <div>
       <div id="map"></div>
+      <b-modal
+        id="formAddMarker"
+        title="Add marker to map"
+        modal-class="my-modal-class"
+        hide-footer
+      >
+        <form>
+          <label>Point name</label>
+          <input type="text" :value="pointName" />
+          <label>Point latitude</label>
+          <input type="text" :value="pointLNG" />
+          <label>Point longitude</label>
+          <input type="text" :value="pointLAT" />
+          <button type="button" @click="closeModal">Cancel</button>
+          <button type="submit" @click.prevent="addMarkerToArray">Save</button>
+        </form>
+      </b-modal>
     </div>
   </div>
 </template>
@@ -11,6 +28,15 @@
 import L from "leaflet";
 
 export default {
+  data() {
+    return {
+      markersArray: [],
+      pointLNG: "",
+      pointLAT: "",
+      pointName: "",
+      coordinates: null,
+    };
+  },
   mounted() {
     const map = L.map("map").setView([48.3794, 31.1656], 6);
 
@@ -20,13 +46,39 @@ export default {
       maxZoom: 18,
     }).addTo(map);
 
+    this.getMarkersArray();
+
+    this.markersArray.forEach((point) => {
+      L.marker([point.lat, point.lng]).addTo(map);
+    });
+
     this.addClickListener(map);
   },
   methods: {
     addClickListener(map) {
       map.on("click", (e) => {
-        L.marker(e.latlng).addTo(map);
+        this.addMarkerToArray(e.latlng);
+
+        this.coordinates = e.latlng;
+
+        this.$bvModal.show("formAddMarker");
       });
+    },
+    getMarkersArray() {
+      if (localStorage.getItem("markers")) {
+        this.markersArray = JSON.parse(localStorage.getItem("markers"));
+      } else {
+        localStorage.setItem("markers", JSON.stringify([]));
+      }
+    },
+    addMarkerToArray() {
+      L.marker(this.coordinates).addTo(map);
+
+      this.pointLAT = this.coordinates.lat.toFixed(3);
+      this.pointLNG = this.coordinates.lng.toFixed(3);
+
+      this.markersArray.push({ name: this.pointName, ...this.coordinates });
+      localStorage.setItem("markers", JSON.stringify(this.markersArray));
     },
   },
 };
